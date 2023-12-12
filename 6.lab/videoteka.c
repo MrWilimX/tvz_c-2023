2af0e003cdffb93d705331e99de9f534416ad51e
MrWilimX <128603539+MrWilimX@users.noreply.github.com>
2023-12-13 00:54:46 +0100

    Update videoteka.c

--- 6.lab/videoteka.c
+++ 6.lab/videoteka.c
@@ -1,68 +1,91 @@
-/*
-TEKST ZADATKA I TEST CASEOVI: https://github.com/emanuelkufrin/tvz_c-2023/blob/d3c190ca70691ad94ec6009b7adc948c8a532e8c/tekstovi%20i%20test%20caseovi/%5B6%5DVideoteka.pdf
-*/
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
+void pronadjiFilm(char** polica,
+	int r,
+	int s,
+	int* redakPronadenog,
+	int* stupacPronadenog) 
+{
+	
+	for (int i = 0; i < r; i++) {
+		for (int j = 0; j < s; j++) {
+			if (polica[i][j] == 'N' &&
+				polica[i][j + 1] == 'N' &&
+				polica[i][j + 2] == 'N') 
+			{
 
-void pronadjiFilm(char** polica, int n, int m);
+				redakPronadenog[0] = i;
+
+				stupacPronadenog[0] = j;
+				stupacPronadenog[1] = j + 1;
+				stupacPronadenog[2] = j + 2;
+
+			}
+		}
+	}
+
+
+}
+
+
 
 int main()
 {
-    int n, m;
-    scanf("%d %d", &n, &m);
+	int n, m;
+	scanf("%d %d", &n, &m);
 
-    char** polica = (char**)malloc(sizeof(char*) * n);
-    for (int i = 0; i < n; i++) {
-        polica[i] = (char*)malloc(sizeof(char) * m);
-    }
+	char** polica = (char**)malloc(n * sizeof(char*));
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf(" %c", &polica[i][j]);
-        }
-    }
+	for (int i = 0; i < n; i++) {
+		polica[i] = (char*)malloc(m * sizeof(char));
+	}
 
-    int dostupno = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (polica[i][j] == 'N' &&
-                polica[i][j + 1] == 'N' &&
-                polica[i][j + 2] == 'N') {
-                dostupno = 1;
-            }
-        }
-    }
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			scanf(" %c", &polica[i][j]);
+		}
+	}
 
-    if (dostupno) {
-        printf("Posudba filmova:");
-        pronadjiFilm(polica, n, m);
-    }
-    else {
-        printf("Sve smo skoro gledali :(");
-    }
-    
-    for (int i = 0; i < n; i++) {
-        free(polica[i]);
-    }
-    free(polica);
+	int redakPronadjenog[1] = { 0 };
+	int stupacPronadjenog[3] = { 0 };
+	
+	int jeliDostupno = 0;
 
-    return 0;
-}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (polica[i][j]=='N'&&
+				polica[i][j+1]=='N'&&
+				polica[i][j+2]=='N'
+				) 
+			{
+				jeliDostupno = 1;
+			}
 
-void pronadjiFilm(char** polica, int n, int m) {
-    int posudeni = 0;
-    for (int i = n - 1; i > 0; i--) {
-        for (int j = m - 1; j > 0; j--) {
-            if (polica[i][j] == 'N' &&
-                polica[i][j - 1] == 'N' &&
-                polica[i][j - 2] == 'N' &&
-                posudeni == 0) {
-                printf(" [%d, %d], [%d, %d] i [%d, %d]", i + 1, j - 1, i + 1, j, i + 1, j + 1);
-                posudeni = 1;
-            }
-        }
-    }
+		}
+	}
+
+	if (jeliDostupno) {
+
+		pronadjiFilm(polica, n, m, &redakPronadjenog, &stupacPronadjenog);
+		printf("\n\n\nPosudba filmova: [%d, %d], [%d, %d] i [%d, %d]\n\n\n", 
+			redakPronadjenog[0]+1, stupacPronadjenog[0]+1,
+			redakPronadjenog[0]+1, stupacPronadjenog[1]+1,
+			redakPronadjenog[0]+1, stupacPronadjenog[2]+1
+			);
+	}
+	else
+	{
+		printf("Sve smo skoro gledali :(");
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		free(polica[i]);
+	}
+
+	free(polica);
+
+	return 0;
 }
